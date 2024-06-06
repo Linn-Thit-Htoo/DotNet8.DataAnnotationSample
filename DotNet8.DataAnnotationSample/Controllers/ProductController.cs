48f92d69e949d9d499881ffd5f12650ddb08603d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-06 20:58:21 +0630

    Add project files.

---
+++ DotNet8.DataAnnotationSample/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using DotNet8.DataAnnotationSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.DataAnnotationSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] ProductModel productModel)
+        {
+            return Ok(productModel);
+        }
+    }
+}
---
+++ DotNet8.DataAnnotationSample/Models/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet8.DataAnnotationSample.Models
+{
+    public class ProductModel
+    {
+        public long ProductId { get; set; }
+
+        [Length(5, 10)]
+        public required string ProductName { get; set; }
+        public decimal Price { get; set; }
+
+        [AllowedValues("Computer", "Mobile", "Beauty")]
+        public string Category { get; set; }
+
+        [DeniedValues("Printers")]
+        public string SubCategory { get; set; }
+
+    }
+}
