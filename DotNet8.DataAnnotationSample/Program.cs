4d680d2a43e51361bd9d3ad19932e9640f78a3c7
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-06 21:00:48 +0630

    -

--- DotNet8.DataAnnotationSample/Program.cs
+++ DotNet8.DataAnnotationSample/Program.cs
@@ -1,7 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers().AddJsonOptions(opt =>
 {
     opt.JsonSerializerOptions.PropertyNamingPolicy = null;
